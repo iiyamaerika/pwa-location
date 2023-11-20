7934d6c52b7f8bcc7899a4c937e1f92376f07562
vitals\er-iiyama <er-iiyama@vitals.co.jp>
2023-11-20 09:54:57 +0900

    エラー処理追加

--- js/index.js
+++ js/index.js
@@ -2,18 +2,20 @@
 navigator.geolocation.watchPosition( (position) => {
   var lat  = position.coords.latitude;            // 緯度を取得
   var lng  = position.coords.longitude;           // 経度を取得
-  var accu = position.coords.accuracy;            // 緯度・経度の精度を取得
-  displayData(lat, lng, accu);                    // displayData 関数を実行
-}, (error) => {                                     // エラー処理（今回は特に何もしない）
+  var acc = position.coords.accuracy;            // 緯度・経度の精度を取得
+  displayData(lat, lng, acc);                    // displayData 関数を実行
+}, (error) => {
+  alert('GPS情報が取得できません．権限を確認してください')
 }, {
-  enableHighAccuracy: true                        // 高精度で測定するオプション
+  enableHighAccuracy: true
 });
+
 
 // データを表示する displayData 関数
 
 
-function displayData(lat, lng, accu) {
+function displayData(lat, lng, acc) {
   var txt = document.getElementById("txt");       // データを表示するdiv要素の取得
   txt.innerHTML = "緯度, 経度: " + lat + ", " + lng + "<br>"  // データ表示
-                + "精度: "       + accu;
+                + "精度: "       + acc;
 }
